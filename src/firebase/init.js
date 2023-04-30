3e745b75e405376430f341063908438f80c50280
Hatem Soliman <hatemthedev@gmail.com>
2023-04-30 03:01:05 +0300

    Added Firebase Hosting & GitHub Integration

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwgsukxKGU2ifKCOrE-oSAE2oq3tSR3JE",
+  authDomain: "library-ecommerce-egypt.firebaseapp.com",
+  projectId: "library-ecommerce-egypt",
+  storageBucket: "library-ecommerce-egypt.appspot.com",
+  messagingSenderId: "269058702858",
+  appId: "1:269058702858:web:f71a4b053e434db9f5a9f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
